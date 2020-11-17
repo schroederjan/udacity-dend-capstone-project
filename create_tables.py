a24355ce1cb9ff1fbe15765ba43b622a692e7512
AionosChina <schroeder_jan@posteo.de>
2020-11-17 17:09:34 +0100

    added new table

--- create_tables.py
+++ create_tables.py
@@ -1,6 +1,11 @@
+
+#IMPORTS
+
 import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries, drop_extensions_queries, create_extensions_queries
+
+#FUNCTIONS
 
 def drop_tables(cur, conn):
     """uses database connection to reset tables in db by droping them, first"""
@@ -17,16 +22,24 @@ def create_tables(cur, conn):
 
 
 def main():
-    """reads connection data from .cfg file and connects with psycopg2"""
-    config = configparser.ConfigParser()
-    config.read('dwh.cfg')
-
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['TIMESCALEDB'].values()))
-    cur = conn.cursor()
-
+    
+    #CONNECTION
+    try:    
+        """reads connection data from .cfg file and connects with psycopg2"""
+        config = configparser.ConfigParser()
+        config.read('dwh.cfg')
+        conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['TIMESCALEDB'].values()))
+        cur = conn.cursor()      
+        
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        sys.exit(1) 
+    
+    #EXTENSIONS
     drop_extensions_queries
     create_extensions_queries
     
+    #TABLES
     drop_tables(cur, conn)
     create_tables(cur, conn)
 
