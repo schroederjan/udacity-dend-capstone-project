fd902c36c66774c9451e8b936ba7f95bff0425fb
AionosChina <schroeder_jan@posteo.de>
2020-11-17 19:09:14 +0100

    changed key

--- aws-vm-timescaledb.sh
+++ aws-vm-timescaledb.sh
@@ -6,12 +6,12 @@ SUBNETID="$(aws ec2 describe-subnets --filters "Name=vpc-id, Values=$VPCID" --qu
 SGID="$(aws ec2 create-security-group --group-name tempsecuritygroup --description "Temporary Security Group" --vpc-id "$VPCID" --output text)"
 aws ec2 authorize-security-group-ingress --group-id "$SGID" --protocol tcp --port 22 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id "$SGID" --protocol tcp --port 5432 --cidr 0.0.0.0/0
-INSTANCEID="$(aws ec2 run-instances --block-device-mappings "DeviceName=/dev/sda1, Ebs={VolumeSize=30}" --image-id "$AMIID" --key-name aionos-one --instance-type t2.micro --security-group-ids "$SGID" --subnet-id "$SUBNETID" --query "Instances[0].InstanceId" --output text)"
+INSTANCEID="$(aws ec2 run-instances --block-device-mappings "DeviceName=/dev/sda1, Ebs={VolumeSize=30}" --image-id "$AMIID" --key-name yourkey --instance-type t2.micro --security-group-ids "$SGID" --subnet-id "$SUBNETID" --query "Instances[0].InstanceId" --output text)"
 echo "waiting for $INSTANCEID ..."
 aws ec2 wait instance-running --instance-ids "$INSTANCEID"
 PUBLICNAME="$(aws ec2 describe-instances --instance-ids "$INSTANCEID" --query "Reservations[0].Instances[0].PublicDnsName" --output text)"
 echo "$INSTANCEID is accepting SSH connections under $PUBLICNAME"
-echo "ssh -i aionos-one.pem ec2-user@$PUBLICNAME"
+echo "ssh -i yourkey.pem ec2-user@$PUBLICNAME"
 read -r -p "Press [Enter] key to terminate $INSTANCEID ..."
 aws ec2 terminate-instances --instance-ids "$INSTANCEID"
 echo "terminating $INSTANCEID ..."
